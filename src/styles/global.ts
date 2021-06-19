ee8540240c877f810bd69f7c14a507ba0d514c3e
Luiz Henrique <58401291+MogLuiz@users.noreply.github.com>
2021-06-19 13:06:23 -0300

    Summary Component

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -34,7 +34,7 @@ export const GlobalStyle = createGlobalStyle `
     }
 
     body {
-        background: var(---background);
+        background: var(--background);
     }
 
     body, input, textarea, button {
