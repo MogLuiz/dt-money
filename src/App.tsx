a2b747681c7071b1c41b647e1fa8b5717c2f22a5
Luiz Henrique <58401291+MogLuiz@users.noreply.github.com>
2021-06-18 22:39:57 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { GlobalStyle } from "./styles/global";
 
 
 export function App() {
   return (
     <div className="App">
       <h1>Olá Mundo</h1>
+      <GlobalStyle />
     </div>
   );
 }
