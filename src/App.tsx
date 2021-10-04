96b2eaa6c08faed97181505ea393b5de479253f5
Luiz Henrique <luizhjramos@outlook.com>
2021-10-04 12:53:34 -0300

    feat: git sintonize

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Modal from 'react-modal'
+import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
@@ -7,34 +7,29 @@ import { TransactionsProvider } from "./TransactionsContext";
 
 import { GlobalStyle } from "./styles/global";
 
-
-Modal.setAppElement('#root')
+Modal.setAppElement("#root");
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransacionModalOpen] =
+    useState(false);
 
+  function handleOpenNewTransactionModal() {
+    setIsNewTransacionModalOpen(true);
+  }
 
-  const [isNewTransactionModalOpen, setIsNewTransacionModalOpen] = useState(false)
-
-    function handleOpenNewTransactionModal() {
-        setIsNewTransacionModalOpen(true)
-    }
-
-    function handleCloseNewTransactionModal() {
-        setIsNewTransacionModalOpen(false)
-    } 
-
+  function handleCloseNewTransactionModal() {
+    setIsNewTransacionModalOpen(false);
+  }
 
   return (
-    <TransactionsProvider >
-      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
-      <NewTransactionModal 
-          isOpen={isNewTransactionModalOpen} 
-          onRequestClose={handleCloseNewTransactionModal}
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle />
     </TransactionsProvider>
   );
 }
-
-
--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -1,14 +1,13 @@
-import { createContext, useEffect, useState, ReactNode } from 'react'
-import { api } from './services/api'
-
+import { createContext, useEffect, useState, ReactNode } from "react";
+import { api } from "./services/api";
 
 interface ITransaction {
-    id: number;
-    title: string;
-    amount: number;
-    type: string;
-    category: string;
-    createdAt: string;
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string;
 }
 
 // interface ITransactionInput {
@@ -18,54 +17,46 @@ interface ITransaction {
 //     category: string;
 // } //Posso fazer o tipo dos inputs assim
 
-type ITransactionInput = Omit<ITransaction, 'id' | 'createdAt'> 
+type ITransactionInput = Omit<ITransaction, "id" | "createdAt">;
 
 //type ItransactionInput = Pick<ITransaction, 'title' | 'amout' | 'type' | 'category'>;
 
 interface ItransactionsProviderProps {
-    children : ReactNode;
+  children: ReactNode;
 }
 
 interface ITransactionsContextData {
-    transactions: ITransaction[];
-    createTransaction: (transaction: ITransactionInput) => Promise<void>;
+  transactions: ITransaction[];
+  createTransaction: (transaction: ITransactionInput) => Promise<void>;
 }
 
-
-
 export const TransactionsContext = createContext<ITransactionsContextData>(
-    {} as ITransactionsContextData
-)
-
-
+  {} as ITransactionsContextData
+);
 
 export function TransactionsProvider({ children }: ItransactionsProviderProps) {
-    
-    const [transactions, setTransactions] = useState<ITransaction[]>([])
+  const [transactions, setTransactions] = useState<ITransaction[]>([]);
 
-    useEffect(() => {
-        api.get('transactions')
-        .then(response => setTransactions(response.data.transactions))
-    },[])
+  useEffect(() => {
+    api
+      .get("transactions")
+      .then((response) => setTransactions(response.data.transactions));
+  }, []);
 
-    async function createTransaction(transactionInput : ITransactionInput){
+  async function createTransaction(transactionInput: ITransactionInput) {
+    const response = await api.post("/transactions", {
+      ...transactionInput,
+      createdAt: new Date(),
+    });
 
-        const response = await api.post("/transactions", {
-            ...transactionInput,
-            createdAt: new Date()
-        })
+    const { transaction } = response.data;
 
-        const { transaction } = response.data
+    setTransactions([...transactions, transaction]);
+  }
 
-        setTransactions([
-            ...transactions,
-            transaction,
-        ])
-    }
-
-    return (
-        <TransactionsContext.Provider value={{ transactions , createTransaction}}>
-            { children }
-        </TransactionsContext.Provider> 
-    )
+  return (
+    <TransactionsContext.Provider value={{ transactions, createTransaction }}>
+      {children}
+    </TransactionsContext.Provider>
+  );
 }
